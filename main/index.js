b58ab6fdf47cc9b82d471e10e633f189bcf27f80
张健 <jian.zhang@chiefclouds.com>
2024-04-25 17:12:55 +0800

    clg3

--- main/index.js
+++ main/index.js
@@ -16,7 +16,7 @@ import render from './render/VueRender'
 
 
 function genActiveRule(routerPrefix) {
-  console.log('routerPrefix2', routerPrefix)
+  console.log('routerPrefix3', routerPrefix)
   return location => location.pathname.startsWith(routerPrefix);
 }
 
